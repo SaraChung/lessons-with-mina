a5c7b3ab8800ea15ad2bf47990c2e25a5d4169b9
Sara <rrrrrah@gmail.com>
2014-10-10 10:05:52 -0400

    added 3 more pictures and changed the title

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'memes/index'
+  root to: 'memes#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
